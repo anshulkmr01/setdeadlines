8c0edc3be5ed4d45f4b4bfab2df710e4849ae011
anshulkmr01 <anshulkmr01@gmail.com>
2020-02-16 01:22:49 +0530

    s

--- application/controllers/user/MainController.php
+++ application/controllers/user/MainController.php
@@ -274,10 +274,11 @@
 		}
 		function saveCase(){
 			$caseData = $this->input->post();
-
+			$i = 0;
 			foreach ($caseData['deadlineData'] as $deadlines) {
+				$i++;
 					 $deadlines = explode ("/amg/", $deadlines);
-						$caseData['eventId'] = $this->saveEventInGoogle($deadlines[0],date('Y-m-d', strtotime($deadlines[2])));
+						$caseData['deadlineData'][$i] = $this->saveEventInGoogle($deadlines[0],date('Y-m-d', strtotime($deadlines[2])));
 					}
 
 					echo "<pre>";
